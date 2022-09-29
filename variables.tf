6a3371bcc542ea10ba1d7fffdabb4cd687333c53
Roberth Strand <me@robstr.dev>
2022-09-29 12:23:19 +0200

    feat(variables): os_type added
    Created a variable for selecting the OS that the app service will be deployed with. This variable makes sure that we deploy the appropriate resource depending on the OS.
    
    Signed-off-by: Roberth Strand <me@robstr.dev>

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "os_type" {
+  description = <<EOT
+  (Optional) Which OS do you want to deploy your App Service with?
+  Can be either `Linux` or `Windows`, defaults to `Linux`.
+  EOT
+  type        = string
+  default     = "Linux"
+  validation {
+    condition     = contains(["Linux", "Windows"], var.os_type)
+    error_message = "This variable only accepts the string Linux or Windows."
+  }
+}
