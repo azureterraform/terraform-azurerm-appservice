db9e8efc9de7fc455f44462d61b4d5e7fb24331d
Roberth Strand <me@robstr.dev>
2022-09-29 12:23:52 +0200

    docs: added flavor text
    Signed-off-by: Roberth Strand <me@robstr.dev>

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 /*
 * # terraform-azurerm-appservice
 *
-* This module aims to be easy to use, flexible, while give the user the guidelines to choose secure options for deploying Azure App Services
+* This module aims to be easy to use, flexible, while give the user the guidelines to choose secure options for deploying Azure App Services.
 */
+
