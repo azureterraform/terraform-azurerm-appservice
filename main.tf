d4540930186896d5e07de718b51fdfe8cceb8118
Roberth Strand <me@robstr.dev>
2022-09-29 16:51:45 +0200

    feat(service-plan): added optional attributes
    Added all the optional attributes that were present in the variable, as well as the conditional to allow the os_type to get overwritten if the user define windows_container as true.
    
    Signed-off-by: Roberth Strand <me@robstr.dev>

--- main.tf
+++ main.tf
@@ -10,7 +10,12 @@ resource "azurerm_service_plan" "main" {
   name                = format("plan-%s", var.name)
   resource_group_name = var.resource_group_name
   location            = var.location
-  os_type             = var.os_type
+  os_type             = var.service_plan.windows_container != false ? "WindowsContainer" : var.os_type
+  sku_name            = var.service_plan.sku_name
 
-  sku_name = var.service_plan.sku_name
+  worker_count                 = var.service_plan.worker_count
+  maximum_elastic_worker_count = var.service_plan.maximum_elastic_worker_count
+  app_service_environment_id   = var.service_plan.app_service_environment_id
+  per_site_scaling_enabled     = var.service_plan.per_site_scaling_enabled
+  zone_balancing_enabled       = var.service_plan.zone_balancing_enabled
 }
