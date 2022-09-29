1eb8e397ae212f753c5121dff96a9af60d77ffe8
Roberth Strand <me@robstr.dev>
2022-09-29 17:59:11 +0200

    docs: update example code
    Signed-off-by: Roberth Strand <me@robstr.dev>

--- examples/appservice.tf
+++ examples/appservice.tf
@@ -1,5 +1,6 @@
 module "appservice" {
-  source = "azureterraform/appservice/azurerm"
+  source  = "azureterraform/appservice/azurerm"
+  version = "1.0.0"
 
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
@@ -8,6 +9,7 @@ module "appservice" {
   os_type = "Linux"
 
   service_plan = {
-    sku_name = "B1"
+    sku_name     = "B1"
+    worker_count = 1
   }
 }
