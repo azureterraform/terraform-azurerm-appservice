cc53060e20fd8b28379b6cdf42ed533b2de690b0
Roberth Strand <me@robstr.dev>
2022-09-29 16:26:54 +0200

    refactor(variables): validation on service_plan
    Added a validation to make sure that the sku_name attribute on the service_plan variable get the correct value.
    
    Signed-off-by: Roberth Strand <me@robstr.dev>

--- variables.tf
+++ variables.tf
@@ -2,6 +2,17 @@ variable "name" {
   description = "Name used with resources deployed with this module."
   type        = string
 }
+
+variable "resource_group_name" {
+  description = "The resource group used for the resources."
+  type        = string
+}
+
+variable "location" {
+  description = "The location the resources will be deployed to."
+  type        = string
+}
+
 variable "service_plan" {
   description = <<EOT
   This object describes the service plan used for your app services. If you want to deploy a windows container, please set windows_container to true.
@@ -17,7 +28,20 @@ variable "service_plan" {
     worker_count                 = optional(number)
     per_site_scaling_enabled     = optional(bool, false)
   })
+  validation {
+    condition = contains([
+      "B1", "B2", "B3", "D1", "F1", "I1", "I2", "I3",
+      "I1v2", "I2v2", "I3v2", "P1v2", "P2v2", "P3v2",
+      "P1v3", "P2v3", "P3v3", "S1", "S2", "S3", "SHARED",
+      "EP1", "EP2", "EP3", "WS1", "WS2", "WS3", "Y1"
+    ], var.service_plan.sku_name)
+    error_message = <<EOT
+    The sku_name attribute has to contain one of the following:
+    B1, B2, B3, D1, F1, I1, I2, I3, I1v2, I2v2, I3v2, P1v2, P2v2, P3v2, P1v3, P2v3, P3v3, S1, S2, S3, SHARED, EP1, EP2, EP3, WS1, WS2, WS3, or Y1.
+    EOT
+  }
 }
+
 variable "os_type" {
   description = <<EOT
   (Optional) Which OS do you want to deploy your App Service with?
